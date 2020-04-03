24c45c13df4953df3fec73d979702d6db4538fe1
mirsandiharyo <mirsandiharyo@gmail.com>
2020-04-03 18:55:00 +0200

    Added: - initialization of variables (velocity, pressure, and force) - creation of staggered grid

--- main.m
+++ main.m
@@ -1,8 +1,8 @@
 %% 
 % A two-dimensional gas-liquid multiphase flows using a front-tracking type
-% method. A Navier-Stokes equation is solved and the fluid properties are 
-% advected by a front-tracking scheme. The code can be used to simulate a 
-% bubble rising in a rectangular box.
+% method. A set of Navier-Stokes equation is solved on an eulerian grid and 
+% the fluid properties are advected by a front-tracking (lagrangian) scheme. 
+% The code can be used to simulate a bubble rising in a rectangular box.
 % Created by: Haryo Mirsandi
 clear all;
 
@@ -14,10 +14,19 @@ bubble = struct('rad',{},'x',{},'y',{});
 
 [domain,param,fluid,bubble]=read_input();
 
-%% initialize variables
+%% initialize variables (liquid is at rest in the beginning)
+% horizontal velocities
+[u, u_temp, u_old] = deal(zeros(domain.nx+1, domain.ny+2));
+% vertical velocities
+[v, v_temp, v_old] = deal(zeros(domain.nx+2, domain.ny+1));
+% pressure, force and temporary variables
+[pres, force_x, force_y, temp1, temp2] = deal(zeros(domain.nx+2, domain.ny+2));
+% center velocities (for plotting)
+[u_ctr, v_ctr] = deal(zeros(domain.nx+1, domain.ny+1));
 
-
-%% set the grid
+%% set the grid (staggered)
+grid_x = linspace(-0.5,domain.nx+2-1.5,domain.nx+2)*(domain.lx/domain.nx);
+grid_y = linspace(-0.5,domain.ny+2-1.5,domain.ny+2)*(domain.ly/domain.ny);
 
 %% set the front (gas-liquid interface)
 
