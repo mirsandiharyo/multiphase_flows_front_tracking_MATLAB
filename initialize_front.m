6985909a9faaf2924d7da99fb7aa1c89c1e9af9d
mirsandiharyo <mirsandiharyo@gmail.com>
2020-04-05 13:24:15 +0200

    Fixed bug in initialize front

--- initialize_front.m
+++ initialize_front.m
@@ -1,9 +1,10 @@
 % initialize the front (lagrangian marker points)
 function [bubble] = initialize_front(bubble)
-    % initialize the marker points
+    % initialize the marker points position
     [bubble.x, bubble.x_old, bubble.y, bubble.y_old] = ...
-        deal(zeros(bubble.pnt+2));
-	[bubble.u, bubble.v] = deal(zeros(bubble.pnt+2));
+        deal(zeros(bubble.pnt+2,1));
+    % initialize the marker points velocity
+	[bubble.u, bubble.v] = deal(zeros(bubble.pnt+2,1));
     % determine the location of the initial spherical bubble
     for i=1:bubble.pnt+2
         bubble.x(i) = bubble.cent_x-bubble.rad*sin(2.0*pi*(i-1) ...
