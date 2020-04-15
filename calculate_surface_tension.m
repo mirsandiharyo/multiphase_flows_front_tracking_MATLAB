2ca6acead8335a1b5cf8c75e3f061ebbb06164b9
mirsandiharyo <mirsandiharyo@gmail.com>
2020-04-15 11:27:07 +0200

    Stored the forces in face structure

--- calculate_surface_tension.m
+++ calculate_surface_tension.m
@@ -1,9 +1,9 @@
 % calculate the surface tension force on the lagrangian grid and distribute
 % it to the surrounding eulerian grid cells
-function[center_x, center_y] = calculate_surface_tension(domain, bubble, ...
+function[face_x, face_y] = calculate_surface_tension(domain, bubble, ...
     fluid_prop)
     % initialize the force
-    [center_x, center_y] = deal(zeros(domain.nx+2, domain.ny+2));
+    [face_x, face_y] = deal(zeros(domain.nx+2, domain.ny+2));
     [tan_x, tan_y] = deal(zeros(bubble.pnt+2, bubble.pnt+2));  
     % calculate the tangent vector
     for i=1:bubble.pnt+1
@@ -15,16 +15,16 @@ function[center_x, center_y] = calculate_surface_tension(domain, bubble, ...
     tan_x(bubble.pnt+2) = tan_x(2);
     tan_y(bubble.pnt+2) = tan_y(2);
     
-    % distribute the tangent force to the eulerian grid
+    % distribute the surface tension force to the eulerian grid
     for i=2:bubble.pnt+1
         % force in x-direction
         force_x = fluid_prop(1).sigma*(tan_x(i)-tan_x(i-1));
-        center_x = distribute_lagrangian_to_eulerian(domain, ...
-            center_x, bubble.x(i), bubble.y(i), force_x, 1);
+        face_x = distribute_lagrangian_to_eulerian(domain, ...
+            face_x, bubble.x(i), bubble.y(i), force_x, 1);
         
         % force in y-direction
         force_y = fluid_prop(1).sigma*(tan_y(i)-tan_y(i-1));
-        center_y = distribute_lagrangian_to_eulerian(domain, ...
-            center_y, bubble.x(i), bubble.y(i), force_y, 2);
+        face_y = distribute_lagrangian_to_eulerian(domain, ...
+            face_y, bubble.x(i), bubble.y(i), force_y, 2);
     end   
 end
--- initialize_variables.m
+++ initialize_variables.m
@@ -3,15 +3,13 @@ function [face, center] = initialize_variables(domain)
     % set the grid with staggered arrangement
     face(1).x = linspace(-0.5, domain.nx+2-1.5, domain.nx+2)*domain.dx;
     face(1).y = linspace(-0.5, domain.ny+2-1.5, domain.ny+2)*domain.dy;
-    % variables located at the cell face
     % velocity in x-direction
     [face(1).u, face(1).u_old, face(1).u_temp] = ...
         deal(zeros(domain.nx+1, domain.ny+2));
     % velocity in y-direction
     [face(1).v, face(1).v_old, face(1).v_temp] = ...
         deal(zeros(domain.nx+2, domain.ny+1));
-    % variables located at cell center
-    % pressure, force and temporary variables
-    [center(1).pres, center(1).force_x, center(1).force_y] = ...
+    % pressure and force
+    [center(1).pres, face(1).force_x, face(1).force_y] = ...
         deal(zeros(domain.nx+2, domain.ny+2));
 end
